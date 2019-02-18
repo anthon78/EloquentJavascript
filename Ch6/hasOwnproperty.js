880f58f817586fb386d4642cbf123b4488b432da
anthon78 <economicsstudent2000@gmail.com>
2019-02-17 19:15:18 -0600

    complete hasownproperty exercise

---
+++ Ch6/hasOwnproperty.js
@@ -0,0 +1,7 @@
+//Can you think of a way to call hasOwnProperty on
+//an object that has its own property by that name?
+
+let map = {one: true, two: true, hasOwnProperty: true};
+
+Object.hasOwnProperty.call(map,"one");
+
